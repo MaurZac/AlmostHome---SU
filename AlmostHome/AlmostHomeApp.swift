227085e86af78b6f52fefd0dc5bc7dcd21a4a7ee
MaurZac <maur.mx@gmail.com>
2024-05-16 18:26:00 -0600

    Initial Commit

---
+++ AlmostHome/AlmostHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlmostHomeApp.swift
+//  AlmostHome
+//
+//  Created by Mauricio Zarate Chula on 5/16/24.
+//
+
+import SwiftUI
+
+@main
+struct AlmostHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlmostHome/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlmostHome
+//
+//  Created by Mauricio Zarate Chula on 5/16/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
